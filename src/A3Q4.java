78d2bdc7df14d9ec9775c97d9588c4c1ff114993
awadb3223 <awadb3223@HRH-IC0027318.wrdsb.ca>
2017-10-06 10:01:00 -0400

    Q4 and Q5 created

---
+++ src/A3Q4.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author awadb3223
+ */
+public class A3Q4 {
+
+    public void countingSort (int array[]){
+        
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
---
+++ src/A3Q5.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author awadb3223
+ */
+public class A3Q5 {
+
+    public void insertionSort2 (int array[]){
+        
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
