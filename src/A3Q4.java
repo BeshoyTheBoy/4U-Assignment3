249c7e3ac1627b2285b742537de5c8591d0ab5c9
awadb3223 <awadb3223@HRH-IC0027318.wrdsb.ca>
2017-11-01 10:12:17 -0400

    Tracker array in progress

--- src/A3Q4.java
+++ src/A3Q4.java
@@ -10,17 +10,23 @@
 public class A3Q4 {
 
     public void countingSort(int array[]) {
-
         int num = 0;
 
         //Create tracker array 
         int[] tracker = new int[101];
+        
         //go through input array 
         for (int i = 0; i < array.length - 1; i++) {
             //find number inside array
             array[i] = num;
             //set num to equal position of tracker array then add 1
-            tracker[num] = tracker[num] + 1;
+            tracker[num] = num + 1;
+            num++;
+        }
+        
+        // testing tracker array
+        for (int i = 0; i < tracker.length - 1; i++) {
+            System.out.println(tracker[i]);
         }
     }
 
@@ -31,15 +37,12 @@ public class A3Q4 {
         // TODO code application logic here
         A3Q4 test = new A3Q4();
 
-        int[] array = new int[5];
-        array[0] = 2;
-        array[1] = 1;
-        array[2] = 5;
-        array[3] = 4;
-        array[4] = 6;
-
+        int[] array = new int[101];
         for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
+            //make a random number
+            array[i] = (int) (Math.random() * 101);
         }
+        
+        test.countingSort(array);
     }
 }
